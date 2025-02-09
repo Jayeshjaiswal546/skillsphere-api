b2fd42d3402fc74825f678a6b7a6fc5d1ba0ceaa
Jayeshjaiswal546 <139918759+Jayeshjaiswal546@users.noreply.github.com>
2025-02-10 03:43:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads')); // Serve the 'uploads' folder
-app.use(router);
+// app.use(router);
+app.get('/',(req,res)=>{
+    res.send("Server is running");
+});
 
+app.get('/view',(req,res)=>{
+    res.send("Server is running on view route");
+});
 
 
 app.listen(PORT,()=>{
